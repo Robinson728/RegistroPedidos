4d3c430f15f3db528b56e1078e9ac5e45be450a4
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-19 21:12:31 -0400

    Creando BLL del Modelo Productos.

---
+++ BLL/ProductosBLL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RegistroPedidos.DAL;
+using RegistroPedidos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroPedidos.BLL
+{
+    public class ProductosBLL
+    {
+        public static Productos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Productos productos;
+
+            try
+            {
+                productos = contexto.Productos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return productos;
+        }
+
+        public static List<Productos> GetList(Expression<Func<Productos, bool>> criterio)
+        {
+            Contexto contexto = new Contexto();
+            List<Productos> lista = new List<Productos>();
+
+            try
+            {
+                lista = contexto.Productos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
