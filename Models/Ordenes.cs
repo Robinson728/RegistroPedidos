0bb4b00ac590b7e68a6ba99e2594d4de458b85f9
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-19 20:44:10 -0400

    Creando Modelo Ordenes.

---
+++ Models/Ordenes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroPedidos.Models
+{
+    public class Ordenes
+    {
+        [Key]
+        public int OrdenId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int SuplidorId { get; set; }
+        public double Monto { get; set; }
+    }
+}
