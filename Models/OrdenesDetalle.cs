438b033d9cd2316ff25c309e2b73ed2a1c87b93d
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-19 20:52:27 -0400

    Creando Modelo OrdenesDetalle

---
+++ Models/OrdenesDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroPedidos.Models
+{
+    public class OrdenesDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int OrdenId { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public double Costo { get; set; }
+
+        [ForeignKey("ProductoId")]
+        public virtual Productos DetallesProducto { get; set; }
+    }
+}
