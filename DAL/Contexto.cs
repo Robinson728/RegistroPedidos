584591a31da9218c8026c4f4137f0e4c03546d3b
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-19 20:57:15 -0400

    Creando Contexto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RegistroPedidos.Models;
+
+namespace RegistroPedidos.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Suplidores> Suplidores { get; set; }
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Ordenes> Ordenes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\PedidosDB.db");
+        }
+    }
+}
