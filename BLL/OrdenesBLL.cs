3a259ef4bc1211ee56cd59dd30abe518dcf6de04
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-19 21:28:31 -0400

    Creando BLL de Modelo Ordenes.

---
+++ BLL/OrdenesBLL.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using RegistroPedidos.DAL;
+using RegistroPedidos.Models;
+
+namespace RegistroPedidos.BLL
+{
+    public class OrdenesBLL
+    {
+        public static bool Guardar(Ordenes ordenes)
+        {
+            if (!Existe(ordenes.OrdenId))
+                return Insertar(ordenes);
+            else
+                return Modificar(ordenes);
+        }
+
+        private static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Ordenes.Any(e => e.OrdenId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+
+        private static bool Insertar(Ordenes ordenes)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Ordenes.Add(ordenes);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        private static bool Modificar(Ordenes ordenes)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                //contexto.Database.ExecuteSqlRaw($"Delete from OrdenesDetalle where OrdenId{ordenes.OrdenId}");
+                contexto.Entry(ordenes).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                var ordenes = contexto.Ordenes.Find(id);
+                if (ordenes != null)
+                {
+                    contexto.Ordenes.Remove(ordenes);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Ordenes Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Ordenes ordenes;
+
+            try
+            {
+                ordenes = contexto.Ordenes.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ordenes;
+        }
+
+        public static List<Ordenes> GetList(Expression<Func<Ordenes, bool>> criterio)
+        {
+            Contexto contexto = new Contexto();
+            List<Ordenes> lista = new List<Ordenes>();
+
+            try
+            {
+                lista = contexto.Ordenes.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
