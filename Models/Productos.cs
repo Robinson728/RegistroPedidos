67b7e15bdb70e9e15ee7d9342b389767b55237ce
Robinson728 <77537741+Robinson728@users.noreply.github.com>
2021-06-19 20:41:12 -0400

    Creando Modelo Productos

---
+++ Models/Productos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroPedidos.Models
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public double Costo { get; set; }
+        public int Inventario { get; set; }
+    }
+}
